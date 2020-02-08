08a8cbb7e917d0a75debe3e0f37ff35efb27ee68
sarkwalvein <>
2020-02-08 21:45:12 +0100

    Added episode download limit and many fixes

--- xdcc-series-autodl.py
+++ xdcc-series-autodl.py
@@ -1,9 +1,27 @@
 import os
 import sys
 import yaml
+import re
 #from xdcc_dl.xdcc import download_packs
 from xdcc_dl.xdcc.XDCCClient import XDCCClient
 from xdcc_dl.pack_search import SearchEngines
+
+def prepare_re_string(search_string):
+    search_string = search_string.replace('\\',r'\\' )
+    search_string = search_string.replace('|',r'\|')
+    search_string = search_string.replace('.',r'\.')
+    search_string = search_string.replace('$',r'\$')
+    search_string = search_string.replace('^',r'\^')
+    search_string = search_string.replace('+',r'\+')
+    search_string = search_string.replace('?',r'\?')
+    search_string = search_string.replace('(',r'\(')
+    search_string = search_string.replace(')',r'\)')
+    search_string = search_string.replace('{',r'\{')
+    search_string = search_string.replace('}',r'\}')
+    search_string = search_string.replace('[',r'\[')
+    search_string = search_string.replace(']',r'\]')
+    search_string = search_string.replace('*','.*?')
+    return search_string
 
 # Determine configuration file path
 binary_path = sys.argv[0]
@@ -39,6 +57,7 @@ if not 'series' in config:
 default_preferred_bot = config['defaults']['preferred bot']
 default_download_folder = config['defaults']['download folder']
 default_search_engine = config['defaults']['search engine']
+default_episode_limit = config['defaults'].get('episode download limit per execution', 999)
 
 # Process each entry
 series_dict = config['series']
@@ -68,42 +87,57 @@ for series in series_dict.keys():
             
         preferred_bot = data.get('preferred bot',default_preferred_bot)
         download_folder = data.get('download folder', default_download_folder)
+        episode_limit = data.get('episode download limit per execution', default_episode_limit)
+        episode_download_count = 0
             
-        while True:
-            search_results = search_engine.value.search(data['search string'].format(data['last episode downloaded'] + 1))         
+        while episode_download_count < episode_limit:
+            current_search_string_with_wildcard = data['search string'].format(data['last episode downloaded'] + 1)
+            current_search_string = current_search_string_with_wildcard.replace('*',' ')
+            current_search_string_regex = re.compile(prepare_re_string(current_search_string_with_wildcard))
+            
+            print("Searching {} for {}: ".format(search_engine_name, current_search_string))
+            search_results = search_engine.value.search(current_search_string)         
+            
+            selected_pack = None
             
             if len(search_results) > 0:
-                selected_pack = search_results[0]
                 for result in search_results:
-                    if result.get_bot() == preferred_bot:
+                    if current_search_string_regex.search(result.get_filename()) is not None:
                         selected_pack = result
-                        break
+                        if result.get_bot() == preferred_bot:
+                            break
                         
-                output_name = ''
-                try:
-                    if download_folder != '':
-                        result.set_directory(download_folder)
-                        
-                    client = XDCCClient(
-                        result,
-                        timeout=120,
-                        fallback_channel=None,
-                        throttle=-1
-                    )
-                    output_name = client.download()
-                except:
-                    print("Process aborted", flush = True)
-                    sys.exit()
-                    #print("Error {} downloading: {} from {}".format(sys.exc_info()[0], result.get_filename(), result.get_bot()))
-                
-                if (output_name == 'Failed' or output_name == ''):
-                    print("Download failed: {} from {}".format(result.get_filename(), result.get_bot()))
+                if selected_pack is not None:
+                    output_name = ''
+                    try:
+                        if download_folder != '':
+                            selected_pack.set_directory(download_folder)
+                            
+                        print("Downloading {} into {}".format(str(selected_pack), selected_pack.directory))
+                            
+                        client = XDCCClient(
+                            selected_pack,
+                            timeout=120,
+                            fallback_channel=None,
+                            throttle=-1
+                        )
+                        output_name = client.download()
+                    except:
+                        print("Process aborted", flush = True)
+                        sys.exit()
+                    
+                    if (output_name == 'Failed' or output_name == ''):
+                        print("Download failed: {} from {}".format(selected_pack.get_filename(), selected_pack.get_bot()))
+                    else:
+                        # Update config file
+                        data['last episode downloaded'] += 1
+                        file = open(config_file_path, 'w')
+                        yaml.dump(config, file)
+                        file.close()    
+                        episode_download_count += 1
                 else:
-                    # Update config file
-                    data['last episode downloaded'] += 1
-                    file = open(config_file_path, 'w')
-                    yaml.dump(config, file)
-                    file.close()                
+                    # Last episode available reached
+                    break
                 
             else:
                 # Last episode available reached
